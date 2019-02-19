0ff0e943d342dc8289ab35abce7dc28a26157db2
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2019-02-19 13:20:13 +0800

    module

--- module/{{cookiecutter.project}}/tests/{{cookiecutter.test_package_dir}}/test_{{cookiecutter.module}}.py
+++ module/{{cookiecutter.project}}/tests/{{cookiecutter.test_package_dir}}/test_{{cookiecutter.module}}.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from {{cookiecutter.project}}.constants import *
+from {{cookiecutter.project_name}}.constants import *
 
 import unittest
 import logging
 
-from {{cookiecutter.project}} import cfg
-from {{cookiecutter.project}} import util
-from {{cookiecutter.project}}.util import Error
-from {{cookiecutter.pkg}} import {{cookiecutter.module}}
+from {{cookiecutter.project_name}} import cfg
+from {{cookiecutter.project_name}} import util
+from {{cookiecutter.project_name}}.util import Error
+from {{cookiecutter.pkg_name}} import {{cookiecutter.module}}
 from tests import Object
 
 
